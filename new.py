6c9e3af424ac3f2beafbe9a17f084831f8dc1e12
jainamd05 <jainam.dudhediya123@gmail.com>
2025-07-29 16:35:12 +0530

    inputted

--- new.py
+++ new.py
@@ -1,1 +1,7 @@
 print("Hello, World!")
+
+name = input("Enter your name: ")
+print(f"Hello, {name}!")
+print("This is a new Python script.")
+print("Welcome to the new script!")
+print("Let's add some more functionality.")
